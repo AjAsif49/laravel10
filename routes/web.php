dfaa669236c1519310d3b62babaced1ccd6e7955
ajasif49 <ajasiif@gmail.com>
2023-12-30 23:07:08 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,63 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// $users = DB::table('users')->get();
+
+// $user = DB::table('users')->where('id', 2)->update(['email' => 'enu@gmail.com']);
+// $user = DB::table('users')->where('id', 2)->delete();
+
+//create new user
+
+// $user = DB::insert('insert into users (name, email, password) values(?,?,?)', [
+//     'Salman',
+//     'Salman@gmail.com',
+//     'password',
+// ]);
+
+// $user = DB::table('users')->insert([
+//     'name' => 'asif',
+//     'email' => 'asif@gmail.com',
+//     'password' => 'password'
+// ]);
+
+// $user = User::create([
+//     'name' => 'enan',
+//     'email' => 'enan@gmail.com',
+//     'password' => 'password'
+// ]);
+
+$users = User::get();
+
+
+
+// $user= User::find(3);
+// $user->update([
+//     'email' => 'enan@gmail.com',
+// ]);
+
+// $user= User::find(3);
+// $user->delete();
+
+dd($users);
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
